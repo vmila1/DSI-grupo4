42de7bbc2882c9e955851e824fa40b12afffc052
Vithória Bastos <vitoriacamila1@hotmail.com>
2023-11-08 18:08:47 -0300

    adiciona a tela de HQs Lidos

--- recomendaqs/lib/main.dart
+++ recomendaqs/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recomendaqs/telas/tela_favoritos.dart';
+import 'package:recomendaqs/telas/tela_lidos.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TelaFavoritos(),
+      home: TelaLidos(),
     );
   }
 }
---
+++ recomendaqs/lib/telas/tela_lidos.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class TelaLidos extends StatefulWidget {
+  @override
+  _TelaLidosState createState() => _TelaLidosState();
+}
+
+class _TelaLidosState extends State<TelaLidos> {
+  int totalImagens = 9;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(86, 83, 255, 1),
+        title: Text('HQs Lidas'),
+      ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/telafundo.png',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          ImagensHQ(),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagensHQ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 100 / 150,
+      ),
+      itemCount: 9,
+      padding: const EdgeInsets.all(10.0),
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Image.asset(
+            'assets/images/imagemhq.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+        );
+      },
+    );
+  }
+}
