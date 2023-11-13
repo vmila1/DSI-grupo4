4c099e66195a107f66cb5b99b0e5c0d090e891bd
C4rl05007 <cvdss17@gmail.com>
2023-11-13 02:17:26 -0300

    Criação da tela de perfil com algumas alterações na main

--- recomendaqs/lib/main.dart
+++ recomendaqs/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:recomendaqs/telas/tela_favoritos.dart';
+import 'package:recomendaqs/telas/tela_favoritos.dart' as Favorito;
 import 'package:recomendaqs/telas/tela_lidos.dart';
 import 'package:recomendaqs/telas/tela_login.dart';
 import 'package:recomendaqs/telas/tela_cadastro.dart';
 import 'package:recomendaqs/telas/tela_inicial.dart';
+import 'package:recomendaqs/telas/tela_perfil.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,12 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/login',
+      initialRoute: '/perfil',
       routes: {
         '/lido': (context) => TelaLidos(),
         '/login': (context) => LoginPage(),
         '/cadastro': (context) => CadastroPage(),
         '/inicial': (context) => HomePage(),
+        '/perfil': (context) => TelaPerfil(),
+        '/favoritos': (context) => Favorito.TelaFavoritos()
       },
     );
   }
---
+++ recomendaqs/lib/telas/tela_perfil.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:recomendaqs/telas/tela_lidos.dart';
+import 'package:recomendaqs/telas/tela_favoritos.dart';
+import 'package:recomendaqs/telas/tela_login.dart';
+
+class TelaPerfil extends StatefulWidget {
+  @override
+  _TelaPerfilState createState() => _TelaPerfilState();
+}
+
+class _TelaPerfilState extends State<TelaPerfil> {
+  String senha = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profile_image.jpg'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Nome do Usuário',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Configurações de HQs',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/favoritos');
+                },
+                child: Text('Favoritos'),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Adicione aqui a lógica para navegar para a tela de Preferências
+                },
+                child: Text('Preferências'),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/lido');
+                },
+                child: Text('HQs Lidos'),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Espaço entre o botão "HQs Lidos" e o título "Configurações da Conta"
+            Text(
+              'Configurações da Conta',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            // Título para alterar e-mail
+            Text(
+              'Alterar E-mail',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // Caixa de texto para alterar e-mail
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    onChanged: (text) {
+                      // Adicione aqui a lógica para lidar com a alteração do e-mail
+                    },
+                    style: TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      labelText: 'Alterar E-mail',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      labelStyle: TextStyle(color: Colors.white),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            // Título para alterar senha
+            Text(
+              'Alterar Senha',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // Caixa de texto para alterar senha
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    onChanged: (text) {
+                      senha = text;
+                    },
+                    obscureText: true,
+                    style: TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      labelText: 'Alterar Senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      labelStyle: TextStyle(color: Colors.white),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            // Título para confirmar senha
+            Text(
+              'Confirmar Senha',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // Caixa de texto para confirmar senha
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      labelText: 'Confirmar Senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      labelStyle: TextStyle(color: Colors.white),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Botão "Sair" com cor vermelha
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Cor vermelha
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Sair'),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Botões de navegação
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/inicial');
+                    },
+                    icon: Icon(Icons.home),
+                    label: Text('Início'),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      // Adicione aqui a lógica para navegar para a tela de Buscar
+                    },
+                    icon: Icon(Icons.search),
+                    label: Text('Buscar'),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      // Adicione aqui a lógica para navegar para a tela de Perfil
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Perfil'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+                height:
+                    20), // Espaçamento adicional entre os botões e a borda inferior
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TelaFavoritos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoritos'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de Favoritos'),
+      ),
+    );
+  }
+}
+
+class TelaHQLidos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HQs Lidos'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de HQs Lidos'),
+      ),
+    );
+  }
+}
