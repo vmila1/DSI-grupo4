a12e34b4bf9aa8fb2c057e3f1c5441f1a0892093
LedroDs <leandro87dev@gmail.com>
2023-12-26 23:29:49 -0300

    consertando bug de iniciação

--- recomendaqs/lib/main.dart
+++ recomendaqs/lib/main.dart
@@ -13,6 +13,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
